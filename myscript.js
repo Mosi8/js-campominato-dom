612c330f0d79be2d77f911050cfd8d72ccde1042
Mosi8 <mauriziomosi8@gmail.com>
2021-12-21 18:39:10 +0100

    completo

--- myscript.js
+++ myscript.js
@@ -1,33 +1,42 @@
 // funzioni
 
-function cambioColor(cella){
+function addClickToBoxes(){
+
+    let cella  = document.getElementsByClassName('col-1');
+
     for(i=0; i < cella.length; i++){
-       if (i == 3){
-        cella[i].addEventListener("click", function(){
-            this.classList.add ("bomba");
+
+       cella[i].addEventListener("click", function(){
+                const numeroCella = this.innerHTML;
+                for (let d = 0; d < posBombe.length; d++){
+                    if ( numeroCella == posBombe[d]){
+                        this.classList.add('bomba');
+                    }else{
+                        this.classList.add('active');
+                    }
+                }
+
+                
         })
-       }else{
-            cella[i].addEventListener("click", function(){
-            this.classList.add ("active");
-        })
-       }
+      
     }
 }
 
 
-function generatoreNum(cella) {
-    let numeroRand = parseInt(Math.floor(Math.random()* cella.length));
+function generatoreNum( maxBombs ) {
+    let numeroRand = parseInt(Math.floor(Math.random()* maxBombs ));
     return numeroRand;
 }
 
 
 
+
 let tasto = document.getElementById('play');
 let campo = document.getElementById('campo');
-const posBombe = [];
-const bombe = 16;
+const posBombe  = [];
+const bombe     = 16;
 
-let quadrato = document.getElementsByClassName('col-1');
+
 
 
 // tasto scelta livello 
@@ -41,57 +50,53 @@ function () {
     if (livelloDiff == 1){
         campo.innerHTML = '';
         for (let i = 1; i <= 100; i++){
-            console.log(i);
 
             campo.innerHTML += '<div class="col-1 block d-inline-block" id= "quadrato-' + [i] + '">' + (i) + '</div>';   
         }
 
         while (posBombe.length < bombe) {
-            let numero = generatoreNum(quadrato); 
+            let numero = generatoreNum( 100 ); 
             if (!posBombe.includes(numero))
             posBombe.push(numero);
             
         }
         console.log(posBombe);
-        cambioColor(quadrato);
+        
 
     }else if (livelloDiff == 2){
         campo.innerHTML = '';
         for (let i = 1; i <= 81; i++){
         
-            console.log(i);
             campo.innerHTML += '<div class="col-1 block block_9 d-inline-block">' + (i) + '</div>';
         }
         while (posBombe.length < bombe) {
-            let numero = generatoreNum(quadrato); 
+            let numero = generatoreNum( 81 ); 
             if (!posBombe.includes(numero))
             posBombe.push(numero);
             
         }
         console.log(posBombe);
-        cambioColor(quadrato);
+       
     }else {
         campo.innerHTML = '';
         for (let i = 1; i <= 49; i++){
             
-            console.log(i);
             campo.innerHTML += '<div class="col-1 block block_7 d-inline-block">' + (i) + '</div>';
             
         }
         
         
         while (posBombe.length < bombe) {
-            let numero = generatoreNum(quadrato); 
+            let numero = generatoreNum( 49 ); 
             if (!posBombe.includes(numero))
             posBombe.push(numero);
             
-        }
-        console.log(posBombe);
-        cambioColor(quadrato);
-            
+        }          
         
         
     }
+
+    addClickToBoxes()
 
 
 });
